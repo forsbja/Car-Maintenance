5d66ac6ff3277369f8f35bcb32067a0263a71693
Jake Forsberg <forsbja@gmail.com>
2022-11-05 20:09:00 -0600

    first commit

---
+++ src/main/java/com/jake/CarMaintenance/CarMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.jake.CarMaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMaintenanceApplication.class, args);
+	}
+
+}
